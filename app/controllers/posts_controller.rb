ffe1313a39b5d21af46d9d089c4f9358643e4bea
yishai <yishai5555@gmail.com>
2020-05-09 23:42:31 +0300

    20

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,8 @@ class PostsController < ApplicationController
     def destroy
         @post = Post.find(params[:id])
         @post.delete
+        
+        redirect_to posts_path
     end
 
     def create
